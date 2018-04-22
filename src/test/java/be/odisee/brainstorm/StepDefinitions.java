cc4797b0b04e2b7b224425ac9d66007c25da6428
Tijs De Belie <Tijsdebelie1997@gmail.com>
2018-04-22 23:23:48 +0200

    feature en testen toegevoegd

--- src/test/java/be/odisee/brainstorm/StepDefinitions.java
+++ src/test/java/be/odisee/brainstorm/StepDefinitions.java
@@ -16,11 +16,11 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class StepDefinitions {
-	
+
 	WebDriver driver;
-	
-	@Given("^I am on the page where I can introduce a new person$")
-	public void i_am_on_the_page_where_I_can_introduce_a_new_person() throws Throwable {
+
+	@Given("^I am on the page where I can introduce a new customer$")
+	public void i_am_on_the_page_where_I_can_introduce_a_new_customer() throws Throwable {
 		// voor het gemak zetten we de geckodriver in de root van het project - indien path de backspaces escapen
 		// geckodriver voor Linux, geckodriver.exe voor Windows 64bit
 		// System.setProperty("webdriver.gecko.driver", "geckodriver");
@@ -36,7 +36,7 @@ public class StepDefinitions {
 		driver.findElement(By.id(fieldName)).sendKeys(enteredText);
 	}
 
-	@When("^I press on the Submit button$")
+	@When("^I press on the Save button$")
 	public void i_press_on_the_Submit_button() throws Throwable {
 		driver.findElement(By.name("submit")).click();
 	}
@@ -45,12 +45,12 @@ public class StepDefinitions {
 		String label;
 		String data;
 	}
-	
+
 	@Then("^I should see the following on the screen$")
 	public void i_should_see_the_following_on_the_screen(List<LabelData> checklist) throws Throwable {
 		// wacht tot de juiste pagina geladen is
 		new WebDriverWait(driver, 10).until(ExpectedConditions
-												.textToBePresentInElementLocated(By.tagName("body"), "Details van persoon")); 
+				.textToBePresentInElementLocated(By.tagName("body"), "Details van persoon"));
 
 		String bodyText = driver.findElement(By.tagName("body")).getText();
 		for (LabelData ld: checklist){
@@ -60,7 +60,7 @@ public class StepDefinitions {
 
 	}
 
-	@When("^I click the Lijst van alle personen Link$")
+	@When("^I click the Lijst van alle klanten Link$")
 	public void i_click_the_Home_Link() throws Throwable {
 		driver.findElement(By.linkText("Lijst van alle personen")).click();
 	}
@@ -69,7 +69,7 @@ public class StepDefinitions {
 	public void i_should_see_a_list_containing(String text2bFound) throws Throwable {
 		// wacht tot de juiste pagina geladen is
 		new WebDriverWait(driver, 10).until(ExpectedConditions
-												.textToBePresentInElementLocated(By.tagName("body"), "Lijst van personen")); 
+				.textToBePresentInElementLocated(By.tagName("body"), "Lijst van personen"));
 		String bodyText = driver.findElement(By.tagName("body")).getText();
 		Assert.assertTrue("Did not find this text: "+text2bFound+"\n",bodyText.contains(text2bFound));
 	}
